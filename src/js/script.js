797cee4815ce3c3975d96816b1c6a8f3c4261cf4
Rubendvb <rubenvasquez556@gmail.com>
2023-02-16 00:45:23 -0300

    Adicionando funções

---
+++ src/js/script.js
@@ -0,0 +1,62 @@
+const minutesEl = document.getElementById("minutes");
+const secondsEl = document.getElementById("seconds");
+const millisecondsEl = document.getElementById("milliseconds");
+const startBtn = document.getElementById("startBtn");
+const pauseBtn = document.getElementById("pauseBtn");
+const resumeBtn = document.getElementById("resumeBtn");
+const resetBtn = document.getElementById("resetBtn");
+
+let interval;
+let minutes = 0;
+let seconds = 0;
+let milliseconds = 0;
+let isPaused = false;
+
+startBtn.addEventListener("click", startTimer);
+pauseBtn.addEventListener("click", pauseTimer);
+resumeBtn.addEventListener("click", resumeTimer);
+
+function startTimer() {
+  interval = setInterval(() => {
+    if (!isPaused) {
+      milliseconds += 10;
+
+      if (milliseconds === 1000) {
+        seconds++;
+        milliseconds = 0;
+      }
+
+      if (seconds === 60) {
+        minutes++;
+        seconds = 0;
+      }
+
+      minutesEl.textContent = formaTime(minutes);
+      secondsEl.textContent = formaTime(seconds);
+      millisecondsEl.textContent = formatMilliseconds(milliseconds);
+    }
+  }, 10);
+
+  startBtn.style.display = "none";
+  pauseBtn.style.display = "block";
+}
+
+function pauseTimer() {
+  isPaused = true;
+  pauseBtn.style.display = "none";
+  resumeBtn.style.display = "block";
+}
+
+function resumeTimer() {
+  isPaused = false;
+  pauseBtn.style.display = "block";
+  resumeBtn.style.display = "none";
+}
+
+function formaTime(time) {
+  return time < 10 ? `0${time}` : time;
+}
+
+function formatMilliseconds(time) {
+  return time < 100 ? `${time}`.padStart(3, 0) : time;
+}
